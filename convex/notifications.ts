cdb0c748e99ab7b5097aaef17ff60f4492ec89a3
Raulif <raul4cade@gmail.com>
2025-06-24 17:24:24 +0200

    Add convex db

---
+++ convex/notifications.ts
@@ -0,0 +1,34 @@
+import { query, mutation } from './_generated/server.js';
+import { v } from 'convex/values';
+
+export const post = mutation({
+  args: {
+    user: v.string(),
+    endpoint: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db.insert('notifications', {
+      user: args.user,
+      enpoint: args.endpoint,
+    });
+  },
+});
+
+export const get = query({
+  args: {},
+  handler: async (ctx) => {
+    const response = await ctx.db.query('notifications').collect();
+    return response[0];
+  },
+});
+
+export const put = mutation({
+  args: {
+    id: v.id('notifications'),
+    endpoint: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const { id, endpoint } = args;
+    return await ctx.db.patch(id, { endpoint });
+  },
+});
--- main.ts
+++ main.ts
@@ -1,4 +1,41 @@
-Deno.serve((req) => {
-  console.log('Request',req)
-  return new Response('Hello world')
-})
+import { Application } from 'jsr:@oak/oak/application';
+import { Router } from 'jsr:@oak/oak/router';
+
+import { ConvexClient } from 'npm:convex/browser';
+import { api } from './convex/_generated/api.js';
+
+const convexUrl = Deno.env.get('CONVEX_URL') as string;
+const convex = new ConvexClient(convexUrl);
+const router = new Router();
+
+router.post('/new-subscription', async (ctx) => {
+  try {
+    const body = await ctx.request.body.json();
+
+    const { endpoint, user } = body;
+
+    if (!endpoint || !user) {
+      ctx.response.body = {};
+    } else {
+      const storedUser = await convex.query(api.notifications.get, {});
+      if (!storedUser) {
+        const newUser = await convex.mutation(api.notifications.post, {
+          user: body.user,
+          endpoint: body.endpoint,
+        });
+
+        console.log({ stored: newUser });
+        ctx.response.body = newUser;
+      }
+      ctx.response.body = storedUser;
+    }
+  } catch (_e) {
+    ctx.response.body = {};
+  }
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+await app.listen({ port: 8080 });
+console.log('Listening in port 8080');
