9e802c671f823ec0689079f79ed6192b6d50bad8
Raulif <raul4cade@gmail.com>
2025-07-07 10:45:25 +0200

    Remove setting vapid details

--- send-notification.ts
+++ send-notification.ts
@@ -2,12 +2,6 @@ import webpush from 'npm:web-push';
 
 import type { PushSubscription } from './types.d.ts';
 
-const publicVapidKey = Deno.env.get('PUBLIC_VAPID_KEY') as string;
-const privateVapidKey = Deno.env.get('PRIVATE_VAPID_KEY') as string;
-const serverUrl = Deno.env.get('SERVER_URL') as string;
-
-webpush.setVapidDetails(serverUrl, publicVapidKey, privateVapidKey);
-
 const capitalize = (text: string) =>
   text.charAt(0).toUpperCase() + text.substring(1);
 
